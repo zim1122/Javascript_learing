b35c7bae5d72a3cfa8f92b11a7d6274c2a067a7d
Zim <mdmohtasimbillahzim@gmail.com>
2026-01-27 00:06:14 +0600

    add kor

---
+++ Javascript_code/turnary.js
@@ -0,0 +1,14 @@
+let num1=30;
+let num2=20;
+let result;
+if(num1>num2){
+    result=num1*2;
+}
+else{
+    result=num1+num2;
+}
+console.log(result);
+
+let result2=(num1>num2)?num1*2:num1+num2;
+console.log(result2);
+
