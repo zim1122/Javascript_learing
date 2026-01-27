d7b559484cde03a11e58389a26401ca6d8b78eca
Zim <mdmohtasimbillahzim@gmail.com>
2026-01-27 12:14:47 +0600

    Array in js

---
+++ Javascript_code/js_array/array_basic.js
@@ -0,0 +1,14 @@
+const numbers=[10,20,30,40];
+console.log(numbers[0]);
+
+let fruits=['Apple','Komla','Banana'];
+console.log(fruits[2]);
+console.log(fruits.length);
+fruits.push('orange');
+console.log(fruits.length);
+console.log(fruits);
+console.log(numbers);
+numbers.push(60);
+console.log(numbers);
+numbers.pop(30);
+console.log(numbers);
