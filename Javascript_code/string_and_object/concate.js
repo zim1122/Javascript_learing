0b48be6a7dca7f34bb3ebfbe62ef532bb3999a4b
Zim <mdmohtasimbillahzim@gmail.com>
2026-01-31 20:03:03 +0600

    slice in string

---
+++ Javascript_code/string_and_object/concate.js
@@ -0,0 +1,11 @@
+const first='Abid';
+const last='nabid';
+
+const fullName= first+' '+last;
+console.log(fullName);
+const fullName2=first.concat(' ').concat(last);
+console.log(fullName2);
+
+//includes
+
+console.log(last.includes('d'));
---
+++ Javascript_code/string_and_object/slice2.js
@@ -0,0 +1,15 @@
+const adress='andorkilla';
+const part=adress.slice(2,5);
+console.log(part);
+
+const sentence='I am good and hardworking student.';
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+// const friendStr='Rahim,karim,dahim,lahim,fahim,sahim';
+// const friends=friendStr.split(',');
+// console.log(friends);
+
+const realFriend = [ 'Rahim', 'karim', 'dahim', 'lahim', 'fahim', 'sahim' ];
+console.log(realFriend.join());
+console.log(realFriend.join('|'));
+console.log(realFriend.join('-'));
