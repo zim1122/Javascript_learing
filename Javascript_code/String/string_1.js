b4913d1a4ef2f8915f999638bc8c992ae4ae4e01
Zim <mdmohtasimbillahzim@gmail.com>
2026-01-31 19:00:47 +0600

    String is immutable and array is mutable

---
+++ Javascript_code/String/string_1.js
@@ -0,0 +1,34 @@
+// const country='Bangladesh';
+// const divsion="Newkhale";
+// const distrcit="B-Baria";
+// // const thana=new String('Daulatpur');
+// // console.log(typeof thana);
+// const capital='Dha ka'
+// console.log(capital.length);
+// console.log(capital[0]);
+
+// //Array is mutable -->You can chnage the array
+// //String is immuatble-->You cannot chnage the array
+// capital[0]='f';
+// console.log(capital);
+
+const school='RAJ UK Uttara Model School';
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+// const subject='chemistry';
+// const book='chemistry';
+// if(subject==book){
+//     console.log('I am reading book aibar exam e fatai felbo');
+// }
+// else{
+//     console.log('Mara Khabo');
+// }
+
+const drink='water ';
+const liquid=' water';
+if(drink.trim()==liquid.trim()){
+    console.log("Pani er opor name life");
+}
+else{
+    console.log('Somudre pani ache khaite pari na');
+}
---
+++ Javascript_code/string_and_object/L_1.js
@@ -0,0 +1,23 @@
+const country='Bangladesh'
+const divison="NewKhale";
+const district='B-Baria';
+const thana=new String('Demra');
+
+// console.log(thana);
+const numbers=[54,98,78,21,65];
+console.log(numbers);
+numbers[1]=101;
+// console.log(numbers.length);
+// console.log(numbers[1]);
+console.log(numbers);
+const capital='Dhaka';
+// console.log(capital.length);
+// console.log(capital[2]);
+console.log(capital);
+capital[0]='F';
+console.log(capital);
+
+/**
+ * String is immutable;Immuatble means not changeable
+ * And Array is Mutable;-->You can chage the array
+ */
