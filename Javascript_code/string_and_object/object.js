f5bdc2cb5e5d395ccfa96f80e659af522d8e44ab
Zim <mdmohtasimbillahzim@gmail.com>
2026-02-01 20:07:41 +0600

    Object in Javascript

---
+++ Javascript_code/string_and_object/object.js
@@ -0,0 +1,54 @@
+const age=21;
+const school='rifle square';
+const isPassed=true;
+let isDeveloper;
+const subjects=['Bangla','English','Physics','Math']
+// console.log(isDeveloper);
+
+// const subject=['bangla','English','Physics','Math'];
+// const bottle_wrong=['white',45];
+// const bottleColor='White';
+// const bottlePrice =45;
+// const bottleDrinks='water';
+
+//declaration of object:
+// const bottle= {
+//     brand: 'Apple',
+//     price:45,
+//     color:'White',
+//     isClean:false
+// }
+
+// const subject = {
+//     name:'Biology',
+//     teacher:'Rasheda mam',
+//     examDate:'30 feb',
+//     chapter:['first','second','third'],
+//     exams:{
+//         name:'Final Exam',
+//         marks:100
+//     }
+// }
+
+const person = {
+    name: 'sodor uddin',
+    age:25,
+    profession:'developer',
+    salary:25000,
+    married:true
+}
+
+// console.log(person);
+//dot notation 
+//dot symbol diay object er property er value acces kora
+// console.log(person.profession)
+// const income = person.salary;
+// console.log(income);
+
+// console.log(person['age']);
+// const boyos = person['age'];
+// console.log(boyos);
+
+person.salary=30000;
+person['age']=26;
+console.log(person);
