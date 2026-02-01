ed0b14855e3f641a3116fcfea2f6073f97103246
Zim <mdmohtasimbillahzim@gmail.com>
2026-02-01 19:16:22 +0600

    reverse in a javascript

---
+++ Javascript_code/string_and_object/reverse.js
@@ -0,0 +1,17 @@
+const sentence='Iam learning Web Dev.';
+
+let reverse='';
+for(const letter of sentence){
+    // console.log(letter);
+    reverse=letter+reverse;
+}
+// console.log(reverse);
+let rev=' '
+for(let i=0;i<sentence.length;i++){
+    console.log(i);
+    console.log(sentence[i]);
+    rev=letter+rev;
+}
+
+const reversed=sentence.split();
+console.log(reversed);
