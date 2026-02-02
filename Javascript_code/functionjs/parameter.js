fb7dd7293439e5b16a7e851d604e9be0797125eb
Zim <mdmohtasimbillahzim@gmail.com>
2026-02-02 22:55:30 +0600

    function basics

---
+++ Javascript_code/functionjs/parameter.js
@@ -0,0 +1,38 @@
+// function vaatkhao(){
+//     console.log('bosi');
+//     console.log('hat dei');
+//     console.log('khai');
+// }
+// vaatkhao();
+
+function square(number){
+    // console.log(number);
+    console.log('Value of the number parameter',number);
+    const borgo= number*number;
+    console.log('Sqaure',borgo);
+}
+square(4);
+console.log('---------------------------------')
+square(12);
+square(5);
+square(405);
+
+console.log('------------------------------------');
+
+
+function add1(num1,num2){
+    const sum=num1+num2;
+    console.log(sum);
+}
+
+add1(5,71);
+add1(65,31);
+
+console.log('-----------------------------------------')
+
+function add(a,b,c,d){
+    const total=a+b+c+d;
+    console.log(total);
+}
+
+add(1,2,3,4);
